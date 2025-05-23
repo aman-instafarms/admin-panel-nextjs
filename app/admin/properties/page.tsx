e3160c66e52fb3fc4be564458a19fc0dc9502e0b
mostlyaman <amansharma110603@gmail.com>
2025-05-23 14:03:04 +0530

    set order in properties

--- app/admin/properties/page.tsx
+++ app/admin/properties/page.tsx
@@ -56,6 +56,7 @@ export default async function Page({ searchParams }: ServerPageProps) {
   }
 
   const data = await (queryWithFilter ? queryWithFilter : query)
+    .orderBy(properties.propertyCode)
     .limit(limit)
     .offset(offset)
     .catch((err) => {
